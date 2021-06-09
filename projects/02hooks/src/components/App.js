109a0c487f407b6790e923e509d59bcbc5c9ece9
cmulliss <cmulliss@gmail.com>
2021-06-09 14:49:02 +0100

    more hooks

--- projects/02hooks/src/components/App.js
+++ projects/02hooks/src/components/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useState, useCallback } from 'react'
 
 import Title from './Title'
 import Count from './Count'
@@ -11,13 +11,14 @@ const App = () => {
 	const [age, setAge] = useState(10)
 
 	// a fn to increment count
-	const incrementCount = () => {
-		setCount(count + 1)
-	}
+	// each time these 2 fns are called seen as diferent fns, in each case, so rerender, memo can't tell that they are the same fns, , in memory the fns are different on each rerender, need to use useCallback. UseCallback needs to listen for some properties, is called dependency. Can use prvious too. Even better as only rerendered once each time as we are not listening for a dependency }, [count]) or }, [age])
+	const incrementCount = useCallback(() => {
+		setCount((prevCount) => prevCount + 1)
+	}, [])
 
-	const incrementAge = () => {
-		setAge(age + 1)
-	}
+	const incrementAge = useCallback(() => {
+		setAge((prevAge) => prevAge + 1)
+	}, [])
 
 	return (
 		<>
@@ -30,7 +31,6 @@ const App = () => {
 		</>
 	)
 }
-
 export default App
-
 // handleCount is prop for <CountBtn />, same process for <Age/>
+// Age and Count not calling a fn, so not rendering each time when using memo
