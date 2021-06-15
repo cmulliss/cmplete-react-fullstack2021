1289cf56cc52dc5e68f7e871aeae5b0107d4f300
cmulliss <cmulliss@gmail.com>
2021-06-15 15:32:19 +0100

    small app

--- projects/wptbill/src/components/Stage1.js
+++ projects/wptbill/src/components/Stage1.js
@@ -6,16 +6,68 @@ import { MyContext } from '../context'
 const Stage1 = () => {
 	const textInput = useRef()
 	const context = useContext(MyContext)
+	// if changes to true show error message
+	const [error, setError] = useState([false, ''])
 
+	const handleSubmit = (e) => {
+		e.preventDefault()
+		const value = textInput.current.value
+		const validate = validateInput(value)
+
+		if (validate) {
+			// form is valid, add player
+			setError([false, ''])
+			context.addPlayer(value)
+
+			// reset
+			textInput.current.value = ''
+		}
+	}
+
+	const validateInput = (value) => {
+		if (value === '') {
+			setError([true, 'Sorry, you need to add something'])
+			return false
+		}
+		if (value.length <= 2) {
+			setError([true, 'Sorry, you need 3 char at least'])
+			return false
+		}
+		return true
+	}
+
+	console.log(context)
 	return (
 		<div>
-			<Form className='mt-4'>
+			<Form onSubmit={handleSubmit} className='mt-4'>
 				<Form.Group>
 					<Form.Control type='text' placeholder='Add player name' name='player' ref={textInput} />
 				</Form.Group>
+				{error[0] ? <Alert variant='danger'>{error[1]}</Alert> : null}
+
 				<Button className='miami' variant='primary' type='submit'>
 					Add Player
 				</Button>
+				{/* show the list of players */}
+
+				{context.state.players && context.state.players.length > 0 ? (
+					<div>
+						<hr />
+						<div>
+							<ul className='list-group'>
+								{context.state.players.map((item, idx) => (
+									<li key={idx} className='list-group-item d-flex justify-content-between align-items-center list-group-item-action'>
+										{item}
+										<span className='badge badge-danger' onClick={() => context.removePlayer(idx)}>
+											x
+										</span>
+									</li>
+								))}
+							</ul>
+                            
+						</div>
+					</div>
+				) : null}
 			</Form>
 		</div>
 	)
@@ -23,3 +75,5 @@ const Stage1 = () => {
 
 export default Stage1
 
+// access form input by using refs
+// going to set errors on state
--- projects/wptbill/src/context/index.js
+++ projects/wptbill/src/context/index.js
@@ -8,10 +8,34 @@ class MyProvider extends Component {
 		players: [],
 		result: ''
 	}
+	// every time you call addPlayerHandler from other components will trigger this fn and update state.
+	addPlayerHandler = (name) => {
+		this.setState((prevState) => ({
+			players: [...prevState.players, name]
+		}))
+	}
+	// call this fn from the other component, stage1
+	removePlayerHandler = (idx) => {
+		let newArray = this.state.players
+		newArray.splice(idx, 1)
+		this.setState({ players: newArray })
+	}
+
 	render() {
-		return <MyContext.Provider value={{ state: this.state }}>{this.props.children}</MyContext.Provider>
+		return (
+			<MyContext.Provider
+				value={{
+					state: this.state,
+					addPlayer: this.addPlayerHandler,
+					removePlayer: this.removePlayerHandler
+				}}
+			>
+				{this.props.children}
+			</MyContext.Provider>
+		)
 	}
 }
-export { MyProvider, MyContext }
+
+export { MyContext, MyProvider }
 // Provider needs a value, passing just state for now.
 // need to add all the fns
