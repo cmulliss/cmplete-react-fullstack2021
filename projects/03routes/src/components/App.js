229de3b21ecb1ad28d3f54a712ecabfe90e53111
cmulliss <cmulliss@gmail.com>
2021-06-17 17:54:59 +0100

    routing

---
+++ projects/03routes/src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Home from './Pages/Home'
+import Posts from './Pages/Posts'
+import Profile from './Pages/Profile'
+
+const App = () => {
+	return (
+		<BrowserRouter>
+			<Route />
+		</BrowserRouter>
+	)
+}
+
+export default App
---
+++ projects/03routes/src/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+// import { BrowserRouter, Route } from 'react-router-dom'
+
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './components/App'
+
+ReactDOM.render(
+	<React.StrictMode>
+		<App />
+	</React.StrictMode>,
+	document.getElementById('root')
+)
